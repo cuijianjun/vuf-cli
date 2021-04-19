8a592bfb031fb2f2a427ff151cdb60660f7cefd1
Frank <376247934@qq.com>
2021-04-19 09:15:28 +0800

    解析脚手架参数

--- imooc-test/bin/index.js
+++ imooc-test/bin/index.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
 console.log("hello");
+ 
+//注册一个命令 imooc-test init
+const argv = require('process').argv;
+const command = argv[2]
+const options = argv.slice(3)
+let [option,param] = options
+option = option.replace('--','') 
+console.log(options);
+console.log(command);
+
+// 实现参数解析 --version和init --name
+if (command.startsWith('--') || command.startsWith('-')) {
+  const globalOption = command.replace(/--|-/g, '');
+  if (globalOption === 'version' || globalOption === "V") {
+    console.log("1.0.0");
+  }
+}
