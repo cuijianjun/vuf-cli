9c8bceea804d9d2360a1f3b34944aabb3bd66e29
Frank <376247934@qq.com>
2021-04-19 17:20:22 +0800

    lerna 参数api处理

--- imooc-test/bin/index.js
+++ imooc-test/bin/index.js
@@ -1,19 +1,59 @@
 #!/usr/bin/env node
-console.log("hello");
- 
-//注册一个命令 imooc-test init
-const argv = require('process').argv;
-const command = argv[2]
-const options = argv.slice(3)
-let [option,param] = options
-option = option.replace('--','') 
-console.log(options);
-console.log(command);
 
-// 实现参数解析 --version和init --name
-if (command.startsWith('--') || command.startsWith('-')) {
-  const globalOption = command.replace(/--|-/g, '');
-  if (globalOption === 'version' || globalOption === "V") {
-    console.log("1.0.0");
-  }
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { epilogue, require } = require('yargs');
+const dedent = require('dedent');
+const pkg = require('../package.json')
+// const arg = hideBin(process.argv);
+const argv = process.argv.slice(2)
+const context = {
+  imoocVersion: pkg.version
 }
+const cli = yargs(arg);
+cli
+  .usage("Usage : imooc-test [command] <option>")
+  .demandCommand(1, "A command is required")
+  .recommendCommands()
+  .fail((err, msg) => {
+    console.log("err", err);
+  })
+  .strict()
+  .alias("h", "help")
+  .alias("v", "version")
+  .wrap(cli.terminalWidth())
+  .epilogue(dedent`  111
+  111`)
+  .options({
+    debug: {
+      type: 'boolean',
+      describe: 'Bootstrap debug mode',
+      alias: 'd'
+    }
+  })
+  .option("registry", {
+    type: 'string',
+    describe: "Define global registry",
+    alias: "r"
+  })
+  .group(['debug'], "Dev Options:")
+  .group(['registry'], 'Extra Options:')
+  .command("init [name]", "Do init a project", (yargs) => {
+    yargs
+      .option("name", {
+        type: "string",
+        describe: 'name of a project'
+      });
+  }, (argv) => {
+    console.log(argv);
+  })
+  .command({
+    command: 'list',
+    aliases: ['ls', 'la', 'll'],
+    describe: 'List local packages',
+    builder: (yargs) => {
+
+    },
+    handle: (argv) => {console.log(argv);}
+  })
+  .parse(argv, context)
