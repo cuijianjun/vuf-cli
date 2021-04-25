287fce0b7f7ed075143e64e94ccd9caa8c3376ea
Frank <376247934@qq.com>
2021-04-25 18:05:27 +0800

    高级定制命令command

--- imooc-test/bin/index.js
+++ imooc-test/bin/index.js
@@ -47,21 +47,46 @@ program.addCommand(service)
 program
   .command('install [name]', 'install package', {
     executableFile: 'imooc-cli',
-    isDefault: true,
+    // isDefault: true,
     hidden: true
   })
   .alias('i')
 
-  program
-  .arguments('<cmd> [options]')
-  .description('test command', {
-    cmd: 'command to run',
-    options: 'options for command'
-  })
-  .action(function(cmd, options) {
-    console.log(cmd, options);
-  })
+  // program
+  // .arguments('<cmd> [options]')
+  // .description('test command', {
+  //   cmd: 'command to run',
+  //   options: 'options for command'
+  // })
+  // .action(function(cmd, options) {
+  //   console.log(cmd, options);
+  // })
 
+// 高级定制1： 自定义help信息
+program.helpInformation = function() {
+  return ''
+}
+
+program.on('--help', function() {
+  console.log("Your help information");
+})
+
+// 高级定制2 实现debug模式
+program.on('option:debug', function() {
+  if (program.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  }
+  console.log(process.env.LOG_LEVEL);
+})
+
+// 高级定制：对未知命令监听
+program.on('command:*', function(obj) {
+  console.log(obj);
+  console.error("未知的命令:''"+obj[0]);
+  const availableCommands = program.commands.map(cmd => cmd.name())
+  console.log(availableCommands);
+  console.log("可用命令：" + availableCommands.join(','));
+})
 
 program
   .parse(process.argv);
