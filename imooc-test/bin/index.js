8b179774ebcffcb16aaa450ce7bbe564f211b8cf
Frank <376247934@qq.com>
2021-04-26 10:29:51 +0800

    支持es module finish

--- imooc-test/bin/index.js
+++ imooc-test/bin/index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
 
+// 如何让Node 支持ES Module
+// 模块化
+// CMD/AMD/require.js
+
+// Common.js
+// 加载： require()
+// 输出： module.exports /exports.x
+
+//Es module
+// 加载： import
+// 输出： export default/export function/const
+require('../dist/core')
