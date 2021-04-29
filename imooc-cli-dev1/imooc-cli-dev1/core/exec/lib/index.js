409d822e0af94935c080de2e8a474896498c6aa5
Frank <376247934@qq.com>
2021-04-29 10:40:44 +0800

    exists

--- imooc-cli-dev1/imooc-cli-dev1/core/exec/lib/index.js
+++ imooc-cli-dev1/imooc-cli-dev1/core/exec/lib/index.js
@@ -1,40 +1,58 @@
 'use strict';
-const log = require('@imooc-cli-dev1/log')
-const Package = require('@imooc-cli-dev1/package');
 
+const path = require('path');
+const Package = require('@imooc-cli-dev1/package');
+const log = require('@imooc-cli-dev1/log');
+const { exec: spawn } = require('@imooc-cli-dev1/utils');
 
 const SETTINGS = {
-    init: '@imooc-cli/init',
+  init: '@imooc-cli/init',
 };
 
-function exec() {
-    let targetPath = process.env.CLI_TARGET_PATH;
-    const homePath = process.env.CLI_HOME_PATH;
+const CACHE_DIR = 'dependencies';
+
+async function exec() {
+  let targetPath = process.env.CLI_TARGET_PATH;
+  const homePath = process.env.CLI_HOME_PATH;
+  let storeDir = '';
+  let pkg;
+  log.verbose('targetPath', targetPath);
+  log.verbose('homePath', homePath);
+
+  const cmdObj = arguments[arguments.length - 1];
+  const cmdName = cmdObj.name();
+  const packageName = SETTINGS[cmdName];
+  const packageVersion = 'latest';
+
+  if (!targetPath) {
+    targetPath = path.resolve(homePath, CACHE_DIR); // 生成缓存路径
+    storeDir = path.resolve(targetPath, 'node_modules');
     log.verbose('targetPath', targetPath);
-    log.verbose('targetPath', homePath);
-
-    const cmdObj = arguments[arguments.length - 1];
-    const cmdName = cmdObj.name();
-    const packageName = SETTINGS[cmdName];
-    const packageVersion = 'latest';
-
-    if (!targetPath) {
-        // 生成缓存路径
-        targetPath = ''
+    log.verbose('storeDir', storeDir);
+    pkg = new Package({
+      targetPath,
+      storeDir,
+      packageName,
+      packageVersion,
+    });
+    if (await pkg.exists()) {
+      // 更新package
+      await pkg.update();
+    } else {
+      // 安装package
+      await pkg.install();
     }
-
-    console.log(cmdObj.name());
-    const pkg = new Package({
-        targetPath,
-        packageName,
-        packageVersion,
-    })
-    console.log(pkg.getRootFilePath());
-    // 1. targetPath --> modulePath
-    // 2. modulePath --> Package(npm模块)
-    // 3. Package.getRootFile (获取入口文件)
-    // 4. Package.update / Package.install
-
-    // 封装 --> 复用
+  } else {
+    pkg = new Package({
+      targetPath,
+      packageName,
+      packageVersion,
+    });
+  }
+  const rootFile = pkg.getRootFilePath();
+  if (rootFile) {
+      require(rootFile).apply(null, arguments)
+  }
 }
+
 module.exports = exec;
--- imooc-cli-dev1/imooc-cli-dev1/models/package/lib/index.js
+++ imooc-cli-dev1/imooc-cli-dev1/models/package/lib/index.js
@@ -1,8 +1,11 @@
 'use strict';
 const path = require('path')
 const pkgDir = require('pkg-dir').sync;
+const pathExists = require('path-exists').sync;
+const npminstall = require('npminstall');
 const { isObject } = require('@imooc-cli-dev1/utils');
 const formatPath = require('@imooc-cli-dev1/format-path');
+const { getDefaultRegistry, getNpmLatestVersion } = require('@imooc-cli-dev1/get-npm-info');
 
 class Package {
     constructor(options) {
@@ -16,16 +19,42 @@ class Package {
         this.targetPath = options.targetPath;
         // // package的存储路径
         // this.storePath = options.storePath;
+        // 缓存package的路径
+        this.storeDir = options.storeDir
         // package的name
         this.packageName = options.packageName;
         // package的version
         this.packageVersion = options.packageVersion;
     }
 
+    async prepare() {
+        if (this.packageVersion === 'latest') {
+            this.packageVersion = await getNpmLatestVersion(this.packageName);
+          }
+    }
+
     // 判断当前的package是否存在
-    exists() { }
+    async exists() {
+        if (this.storeDir) {
+            await this.prepare();
+        } else {
+            return pathExists(this.targetPath);
+        }
+    }
     // 安装package
-    install() { }
+    install() {
+        npminstall({
+            root: this.targetPath,
+            storeDir: this.storeDir,
+            registry: getDefaultRegistry(),
+            pkgs: [
+                {
+                    name: this.packageName,
+                    version: this.packageVersion
+                }
+            ]
+        })
+    }
     // 更新package
     update() { }
     // 获取入口文件的路径
