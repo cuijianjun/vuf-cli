1b13bbedc4dd276d7c3a948fbc28cfa4f2706482
Frank <376247934@qq.com>
2021-04-29 12:22:24 +0800

    更新模块

--- imooc-cli-dev1/imooc-cli-dev1/models/package/lib/index.js
+++ imooc-cli-dev1/imooc-cli-dev1/models/package/lib/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const path = require('path')
+const fse = require('fs-extra');
 const pkgDir = require('pkg-dir').sync;
 const pathExists = require('path-exists').sync;
 const npminstall = require('npminstall');
@@ -25,18 +26,31 @@ class Package {
         this.packageName = options.packageName;
         // package的version
         this.packageVersion = options.packageVersion;
+        // package的缓存目录前缀
+        this.cacheFilePathPrefix = this.packageName.replace('/', '_');
     }
 
     async prepare() {
+        if (this.storeDir && !pathExists(this.storeDir)) {
+            fse.mkdirpSync(this.storeDir)
+        }
         if (this.packageVersion === 'latest') {
             this.packageVersion = await getNpmLatestVersion(this.packageName);
-          }
+        }
+    }
+    get cacheFilePath() {
+        return path.resolve(this.storeDir, `_${this.cacheFilePathPrefix}@${this.packageVersion}@${this.packageName}`);
+    }
+
+    getSpecificCacheFilePath(packageVersion) {
+        return path.resolve(this.storeDir, `_${this.cacheFilePathPrefix}@${packageVersion}@${this.packageName}`);
     }
 
     // 判断当前的package是否存在
     async exists() {
         if (this.storeDir) {
             await this.prepare();
+            return pathExists(this.cacheFilePath);
         } else {
             return pathExists(this.targetPath);
         }
@@ -56,7 +70,30 @@ class Package {
         })
     }
     // 更新package
-    update() { }
+    async update() {
+        await this.prepare();
+        // 1. 获取最新的npm模块版本号
+        const latestPackageVersion = await getNpmLatestVersion(this.packageName);
+
+        // 2. 查询最新版本号对应的路径是否存在
+        const latestFilePath = this.getSpecificCacheFilePath(latestPackageVersion);
+
+        // 3. 如果不存在，则直接安装最新的版本
+        if (!pathExists(latestFilePath)) {
+            await npminstall({
+                root: this.targetPath,
+                storeDir: this.storeDir,
+                registry: getDefaultRegistry(),
+                pkgs: [{
+                    name: this.packageName,
+                    version: latestPackageVersion,
+                }],
+            });
+            this.packageVersion = latestPackageVersion;
+        } else {
+            this.packageVersion = latestPackageVersion;
+        }
+    }
     // 获取入口文件的路径
     getRootFilePath() {
         // 1. 获取package.json所在的目录 -- pkg-dir
